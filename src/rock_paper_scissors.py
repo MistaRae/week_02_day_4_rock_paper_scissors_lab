2f226056e2966038e17181aef0f3ed7b481ace7c
MistaRae <itsamistarae@gmail.com>
2021-04-08 16:40:19 +0100

    rock paper scissors lizard spock complete and working

--- src/rock_paper_scissors.py
+++ src/rock_paper_scissors.py
@@ -9,15 +9,43 @@ def play_game(player1, player2):
         return "Rock wins!"
     if player1.gesture == "rock" and player2.gesture == "paper":
         return "Paper wins!"
-    if player1.gesture == "paper" and player2.gesture == "rock":
-        return "Paper wins!"
-    if player1.gesture == "paper" and player2.gesture == "scissors":
-        return "Scissors wins!"
+    if player1.gesture == "rock" and player2.gesture == "Spock":
+        return "Spock wins!"
+    if player1.gesture == "rock" and player2.gesture == "lizard":
+        return "Rock wins!"
+
     if player1.gesture == "scissors" and player2.gesture == "paper":
         return "Scissors wins!"
     if player1.gesture == "scissors" and player2.gesture == "rock":
         return "Rock wins!"
-    # if player1.gesture == "rock" and player2.gesture == "paper":
-    #     return "Paper wins!"
-    # if player1.gesture == "rock" and player2.gesture == "paper":
-    #     return "Paper wins!"
+    if player1.gesture == "scissors" and player2.gesture == "Spock":
+        return "Spock wins!"
+    if player1.gesture == "scissors" and player2.gesture == "lizard":
+        return "Scissors wins!"
+
+    if player1.gesture == "paper" and player2.gesture == "Spock":
+        return "Paper wins!"
+    if player1.gesture == "paper" and player2.gesture == "lizard":
+        return "Lizard wins!"
+    if player1.gesture == "paper" and player2.gesture == "rock":
+        return "Paper wins!"
+    if player1.gesture == "paper" and player2.gesture == "scissors":
+        return "Scissors wins!"
+
+    if player1.gesture == "lizard" and player2.gesture == "rock":
+        return "Rock wins!"
+    if player1.gesture == "lizard" and player2.gesture == "scissors":
+        return "Scissors wins!"
+    if player1.gesture == "lizard" and player2.gesture == "Spock":
+        return "Lizard wins!"
+    if player1.gesture == "lizard" and player2.gesture == "paper":
+        return "Lizard wins!"
+
+    if player1.gesture == "Spock" and player2.gesture == "paper":
+        return "Paper wins!"
+    if player1.gesture == "Spock" and player2.gesture == "rock":
+        return "Spock wins!"
+    if player1.gesture == "Spock" and player2.gesture == "scissors":
+        return "Spock wins!"
+    if player1.gesture == "Spock" and player2.gesture == "lizard":
+        return "Lizard wins!"
--- test/test_rock_paper_scissors.py
+++ test/test_rock_paper_scissors.py
@@ -10,30 +10,54 @@ class TestRockPaperScissors(unittest.TestCase):
         self.player_2 = Player("Mark", "scissors")
         self.player_3 = Player("Colin", "paper")
         self.player_4 = Player("Sky", "rock")
-
-    def test_if_gesture1_equals_gesture2(self):
-        self.assertEqual("It's a draw!", play_game(
-            self.player_1, self.player_4))
+        self.player_5 = Player("Hannah", "lizard") 
+        self.player_6 = Player("Roosa", "Spock")
 
     def test_if_its_a_draw(self):
-        self.assertEqual("It's a draw!", play_game(
-            self.player_1, self.player_4))
+        self.assertEqual("It's a draw!", play_game(self.player_1, self.player_4))
 
     def test_if_gesture1_does_not_equal_gesture2(self):
-        self.assertEqual("Rock wins!", play_game(
-            self.player_1, self.player_2))
+        self.assertEqual("Rock wins!", play_game(self.player_1, self.player_2))
 
     def test_draw_is_declared_when_gestures_equal(self):
-        self.assertEqual("It's a draw!", play_game(
-            self.player_1, self.player_4))
+        self.assertEqual("It's a draw!", play_game(self.player_1, self.player_4))
 
     def test_rock_beats_scissors(self):
         self.assertEqual("Rock wins!", play_game(self.player_1, self.player_2))
 
     def test_scissors_beats_paper(self):
-        self.assertEqual("Scissors wins!", play_game(
-            self.player_2, self.player_3))
+        self.assertEqual("Scissors wins!", play_game(self.player_2, self.player_3))
 
     def test_paper_beats_rock(self):
-        self.assertEqual("Paper wins!", play_game(
-            self.player_3, self.player_4))
+        self.assertEqual("Paper wins!", play_game(self.player_3, self.player_4))
+
+################
+#  extensions  #
+################
+
+#rock paper scissors9 lizard spock 
+
+    def test_spock_beats_rock(self):
+        self.assertEqual("Spock wins!", play_game(self.player_6, self.player_1))
+
+    def test_spock_beats_scissors(self):
+        self.assertEqual("Spock wins!", play_game(self.player_6, self.player_2))
+
+    def test_spock_loses_to_lizard(self):
+        self.assertEqual("Lizard wins!", play_game(self.player_6, self.player_5))
+
+    def test_spock_loses_paper(self):
+        self.assertEqual("Paper wins!", play_game(self.player_6, self.player_3))
+
+    def test_lizard_beats_spock(self):
+        self.assertEqual("Lizard wins!", play_game(self.player_5, self.player_6))
+
+    def test_lizard_beats_paper(self):
+        self.assertEqual("Lizard wins!", play_game(self.player_5, self.player_3))
+
+    def test_lizard_loses_to_rock(self):
+        self.assertEqual("Rock wins!", play_game(self.player_5, self.player_1))
+
+    def test_lizard_loses_to_scissors(self):
+        self.assertEqual("Scissors wins!", play_game(self.player_5, self.player_2))
+
